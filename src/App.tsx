ee13d10d12b2daddd6aa6fb1a7c415dacf2bae5f
Leandro <leonunes215@outlook.com>
2022-09-06 18:54:37 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { Router } from "./Router";
 import { GlobalStyles } from "./styles/global";
-import { defaultTheme } from "./styles/theme/DefaultTheme";
+import { defaultTheme } from "./styles/theme/defaultTheme";
 
 export function App() {
 
